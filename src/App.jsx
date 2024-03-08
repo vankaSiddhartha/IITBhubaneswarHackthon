e4008c40af657a4b3a1c1a7853f01d46530620cd
ViniVars <145172391+ViniVars@users.noreply.github.com>
2024-03-09 00:25:04 +0530

    Added Product additon
    You can submit your Product for bidding

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,37 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddCard from './Gojo/AddCard'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBaGEpnwQf01FPH_3FDz8HiKrrFUD6h-lU",
+  authDomain: "hackathon-80b62.firebaseapp.com",
+  projectId: "hackathon-80b62",
+  storageBucket: "hackathon-80b62.appspot.com",
+  messagingSenderId: "105991838384",
+  appId: "1:105991838384:web:bae08d8e166691b927f975",
+  measurementId: "G-H78SJEQ11E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase(app)
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vitet</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AddCard db = {db} />
     </>
   )
 }
